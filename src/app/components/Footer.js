3c71129ee37619fe197ad1d8b19bb0560dfda2dc
Your Name Here <lawsonezekiel68@gmail.com>
2026-02-17 17:30:33 +0100

    Update Footer and add Newsletter component

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,8 +1,19 @@
 // src/app/components/Footer.js
 export default function Footer() {
   return (
-    <footer className="bg-gray-200 text-gray-800 p-4 mt-8 text-center">
-      <p>&copy; {new Date().getFullYear()} NFT Alien. All rights reserved.</p>
-    </footer>
+<footer className="bg-[#000000] ">
+  <div className="rounded-t-xl overflow-hidden px-6 py-16 lg:py-20 lg:px-8 relative">
+    <nav className="flex flex-col md:flex-row flex-wrap justify-center gap-x-12 gap-y-3 text-neutral-100 font-semibold hover:text-white no-underline uppercase" aria-label="Footer">
+      <a href="#" className="">Home</a>
+      <a href="#" className="">About</a>
+      <a href="#" className="">Collection</a>
+      <a href="#" className="">Privacy Policy</a>
+      <a href="#" className="">Contact</a>
+    </nav>
+    <p className="text-center pt-8">&copy; {new Date().getFullYear()} NFT Alien. All rights reserved.</p>
+   
+
+  </div>  
+</footer>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,17 +3,19 @@ import Hero from './components/Hero';
 import HowWeWork from './components/HowWeWork';
 import NFTCollection from './components/NFTCollection';
 import StorySection from './components/StorySection';
+import Newsletter from './components/Newsletter';
 import Footer from './components/Footer';
 
 export default function Home() {
 
   return (
-    <div className=" px-4 font-[family-name:var(--font-geist-sans)]">
+    <div className=" font-[family-name:var(--font-geist-sans)]">
       <Header />
       <Hero />
       <HowWeWork />
       <NFTCollection />
       <StorySection />
+      <Newsletter />
       <Footer />
     </div>
   );
