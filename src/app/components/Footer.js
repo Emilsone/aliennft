0cb93f8d978193dbb1f5649c33bd562d23f3fa7e
Your Name Here <lawsonezekiel68@gmail.com>
2026-02-17 18:32:07 +0100

    Adjust layout and content for Header, Footer, and Newsletter

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,19 +1,23 @@
-// src/app/components/Footer.js
+
+import Image from 'next/image';
 export default function Footer() {
   return (
-<footer className="bg-[#000000] ">
-  <div className="rounded-t-xl overflow-hidden px-6 py-16 lg:py-20 lg:px-8 relative">
-    <nav className="flex flex-col md:flex-row flex-wrap justify-center gap-x-12 gap-y-3 text-neutral-100 font-semibold hover:text-white no-underline uppercase" aria-label="Footer">
-      <a href="#" className="">Home</a>
-      <a href="#" className="">About</a>
-      <a href="#" className="">Collection</a>
-      <a href="#" className="">Privacy Policy</a>
-      <a href="#" className="">Contact</a>
-    </nav>
-    <p className="text-center pt-8">&copy; {new Date().getFullYear()} NFT Alien. All rights reserved.</p>
-   
+    <footer className="bg-[#000000] py-16 lg:py-18 lg:px-8  ">
+      <div className="rounded-t-xl overflow-hidden px-6 relative">
+        <div className=" flex items-center justify-center mb-8">
+          <Image src="/aliennft-logo.svg" alt="AlienNFT Logo" width={170} height={120} className="h-10 w-auto" />
+        </div>
+        <nav className="flex flex-col md:flex-row flex-wrap justify-center gap-x-12 gap-y-3 text-neutral-100 font-semibold hover:text-white no-underline uppercase" aria-label="Footer">
+          <a href="#" className="">Home</a>
+          <a href="#" className="">About</a>
+          <a href="#" className="">Blog</a>
+          <a href="#" className="">Privacy Policy</a>
+          <a href="#" className="">Contact</a>
+        </nav>
+        <p className="text-center pt-8 capitalize">&copy; {new Date().getFullYear()} NFT Alien. All rights reserved.</p>
 
-  </div>  
-</footer>
+
+      </div>
+    </footer>
   );
 }
--- src/app/components/Newsletter.js
+++ src/app/components/Newsletter.js
@@ -1,11 +1,11 @@
 import Image from 'next/image';
 export default function Newsletter() {
     return (
-        <section className=" bg-[#000000]/30">
+        <section className=" bg-[#000000]/45">
             <div className=" py-8 sm:py-24 lg:py-32">
                 <div className="mx-auto grid max-w-7xl grid-cols-1 gap-10 px-6 lg:grid-cols-12 lg:gap-8 lg:px-8">
                     <div className="max-w-xl text-3xl tracking-tight text-white sm:text-4xl lg:col-span-7">
-                        <h2 className="inline sm:block lg:inline xl:block font-bold">Want product news and updates?</h2>
+                        <h2 className="inline sm:block lg:inline xl:block font-bold capitalize">Want product news and updates?</h2>
                         <p className="inline sm:block lg:inline xl:block text-lg  py-2">Sign up for our newsletter.</p>
                     </div>
                     <form className="w-full max-w-md lg:col-span-5 lg:pt-2">
@@ -17,19 +17,19 @@ export default function Newsletter() {
                                 type="email"
                                 autoComplete="email"
                                 required
-                                className="min-w-0 flex-auto  border-0 bg-white/10 px-3.5 py-4 text-white shadow-sm ring-1 ring-inset ring-[#21E786]/10 placeholder:text-white/75 focus:ring-2 focus:ring-inset focus:ring-[#21E786] sm:text-sm sm:leading-6"
+                                className="min-w-0 flex-auto  border-0 bg-white/10 px-3.5 py-3 text-white shadow-sm ring-1 ring-inset ring-[#21E786]/10 placeholder:text-white/75 focus:ring-2 focus:ring-inset focus:ring-[#21E786] sm:text-sm sm:leading-6"
                                 placeholder="Enter your email"
                             />
                             <button
                                 type="submit"
-                                className="flex-none  bg-[#21E786] px-3.5 py-1 text-sm font-semibold text-[#000000] shadow-sm hover:bg-[#21E786] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#21E786]"
+                                className="flex-none  bg-[#21E786] px-3.5  text-sm font-semibold text-[#000000] shadow-sm hover:bg-[#21E786] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#21E786]"
                             >
                                 Subscribe
                             </button>
                         </div>
                         <p className="mt-4 text-sm leading-6 text-gray-300">
                             We care about your data. Read our{" "}
-                            <a href="#" className="font-semibold text-white hover:text-indigo-50">
+                            <a href="#" className="font-semibold text-[#21E786]">
                                 privacy&nbsp;policy
                             </a>.
                         </p>
